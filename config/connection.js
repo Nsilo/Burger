5f0831e5c235352c1b224f9e2723d2ce387d4c57
Jivaka <jivakapyane93@gmail.com>
2021-03-02 00:37:04 -0800

    connections finished

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+
+const connection = mysql.createConnection(
+  process.env.JAWSDB_URL || {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db",
+  }
+);
+
+connection.connect((err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log("connected: ", connection.threadId);
+});
+
+module.exports = connection;
