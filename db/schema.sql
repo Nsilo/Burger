65da894d43b522e14572583a8a6262d82d11034e
Jivaka Nsilo Payne <71672852+Nsilo@users.noreply.github.com>
2021-04-03 10:36:43 -0700

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,13 @@
-* Create the `burgers_db`.
-   * Switch to or use the `burgers_db`.
-   * Create a `burgers` table with these fields:
-     * **id**: an auto incrementing int that serves as the primary key.
-     * **burger_name**: a string.
-     * **devoured**: a boolean.
+
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+	id int NOT NULL AUTO_INCREMENT,
+    burger_name varchar(255) NOT NULL,
+    devoured BOOL DEFAULT false,
+    PRIMARY KEY (id)
+    );
