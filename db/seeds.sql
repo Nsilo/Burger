398c5b2dfc047c4e20b9c26f7b4394d22d966766
Jivaka Nsilo Payne <71672852+Nsilo@users.noreply.github.com>
2021-04-03 10:38:59 -0700

    Update seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ribeye Deluxe', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali Guac', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bleu Ranch', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Burger', FALSE);
